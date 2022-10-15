b8bd1341e8cac97ad7f7155c42484d89e8de4aa8
diuere <86838002+diuere@users.noreply.github.com>
2022-10-15 19:21:36 -0300

    observers functions optimized
    a global function that creates instances of IntersectionObserver was created and implemented on other observer handle functions to minimize code repetition

--- src/store/observers.js
+++ src/store/observers.js
@@ -1,76 +1,60 @@
-export const pageObserver = (page) => { // helps maintaining the page selection if the page is refreshed
-  const navLinks = document.querySelectorAll(".route-selector");
-  
-  const pageObserver = new IntersectionObserver((entries, pageObserver) => {
+const getObserver = (element, activate, deactivate, threshold) => {
+  /* 
+    receives a DOM element; 
+    creates an IntersectionObserver instance; 
+    listen to intersecting changes; 
+    if the intersecting is true, it runs a function passed as argument; 
+    if intersecting is false, it runs a different function passed as argument.
+  */
+  const options = threshold === null ? {} : { threshold: threshold, };
+
+  const elementObserver = new IntersectionObserver((entries, elementObserver) => {
     entries.forEach(entry => {
       if(entry.isIntersecting){
-        navLinks.forEach(link => {
-          const linkClass = link.classList;
-
-          if(linkClass[1] === page.id){
-            link.classList.add("route-selected");
-          } else {
-            link.classList.remove("route-selected");
-          }
-        })
+        if(activate) activate();
+      } else if(!entry.isIntersecting){
+        if(deactivate) deactivate();
       }
     })
-  }, {})
+  }, options);
 
-  pageObserver.observe(page)
+  elementObserver.observe(element);
 }
 
+export const pageObserver = (page) => { // helps maintaining the page selection if the page is refreshed
+  const navLinks = document.querySelectorAll(".route-selector");
+
+  const toggleLinkClass = () => {
+    navLinks.forEach(link => { // should loop through navLinks apply or remove the .route-selected class depending on the section.page that's currently being seen.
+      const linkClass = link.classList;
+
+      if(linkClass[1] === page.id){
+        link.classList.add("route-selected");
+      } else {
+        link.classList.remove("route-selected");
+      }
+    })
+  }
+  
+  getObserver(page, () => toggleLinkClass(), false, null);
+}
 
 export const hamburgerIconObserver = ({ toggleMenu }) => { // close the menu overlay based on the .hamburger-menu-wrapper intersection state
   const hamburgerMenuWrapper = document.querySelector(".hamburger-menu-wrapper");
 
-  const hamburgerMenuObserver = new IntersectionObserver((entries, hamburgerMenuObserver) => {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting){
-        toggleMenu(true);
-      }
-    })
-  }, {})
-
-  hamburgerMenuObserver.observe(hamburgerMenuWrapper);
+  getObserver(hamburgerMenuWrapper, () => toggleMenu(true), false, null);
 }
 
-
-export const headerObserver = ({ toggleMenu }) => { //toggle the .rollback element
+export const headerObserver = ({ toggleMenu }) => { // toggle the .rollback element
   const mainHeader = document.querySelector("#mainHeader");
   const rollbackWrapper = document.querySelector("#rollbackWrapper");
 
-  const headerObserver = new IntersectionObserver((entries, headerObserver) => {
-    entries.forEach(entry => {
-      if(!entry.isIntersecting){
-        toggleMenu(true);
-        rollbackWrapper.classList.add("rollback");
-      } else {
-        rollbackWrapper.classList.remove("rollback");
-      };
-    });
-  }, {
-    threshold: .5,
-  });
-
-  headerObserver.observe(mainHeader);
+  getObserver(mainHeader, () => rollbackWrapper.classList.remove("rollback"), () => { toggleMenu(true); rollbackWrapper.classList.add("rollback"); }, .5);
 }
 
 export const footerObserver = () => { // change the backgroundColor of the .rollback element
-    const mainFooter = document.querySelector('.main-footer');
-    const rollbackIcon = document.querySelector("#rollbackIcon");
+  const mainFooter = document.querySelector('.main-footer');
+  const rollbackIcon = document.querySelector("#rollbackIcon");
 
-    const footerObserver = new IntersectionObserver((entries, footerObserver) => {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting){
-                rollbackIcon.style.cssText = "color: #201d1d;"
-            } else {
-                rollbackIcon.style.cssText = "color: #e9eaeb;"
-            }
-        })
-    }, {
-        threshold: .3,
-    })
-
-    footerObserver.observe(mainFooter);
+  getObserver(mainFooter, () => rollbackIcon.style.cssText = "color: #e9eaeb;", () => rollbackIcon.style.cssText = "color: #201d1d;", .3)
 }
